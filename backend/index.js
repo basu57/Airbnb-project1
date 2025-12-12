d6fbf327ae3be553c407da7730663327f1caac55
Basu Kumar <basukumar94103@gmail.com>
2025-12-12 21:49:28 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
     origin:["https://airbnb1-se5t.onrender.com",   // your deployed frontend
-    "http://localhost:5173" ]
+    "http://localhost:5173" ],
     credentials:true
 }))
 
